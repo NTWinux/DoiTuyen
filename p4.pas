7e4c8a3e78fa1aaa9817f9cff27019767b83bd18
Your Name <dnt2552@gmail.com>
2016-09-17 08:40:41 +0700

    Add New Line

--- p4.pas
+++ p4.pas
@@ -1,5 +1,22 @@
 uses crt;
 // Mang 2 Chieu
+type Mang2Chieu:array[1..10,1..10]of integer;
+var
+    a:Mang2Chieu;
+    m,n,i,j:integer;
 begin
-    
+    writeln('Dong: ');readln(m);
+    writeln('Cot: ');readln(n);
+    for i:=1 to m do
+        for j:=1 to n do
+        begin
+            write('a[',i,',',j,']');readln(a[i,j]);
+        end;
+    for i:=1 to m do
+    begin
+        for j:=1 to n do
+            writeln(a[i,j]:4);
+        writeln;
+    end;
+    readln;
 end.
