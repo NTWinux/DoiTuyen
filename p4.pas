1bab962c96611c8830d64c5b8b2003633f335b40
Your Name <dnt2552@gmail.com>
2016-09-17 08:55:53 +0700

    Add Code

--- p4.pas
+++ p4.pas
@@ -3,20 +3,31 @@ uses crt;
 type Mang2Chieu=array[1..10,1..10]of integer;
 var
     a:Mang2Chieu;
-    m,n,i,j:integer;
+    m,n,i,j,s,k,x,c:integer;
 begin
+    k:=-32768;
     write('Dong: ');readln(m);
     write('Cot: ');readln(n);
+    write('x:=');readln(x);
+    c:=0;s:=0;
     for i:=1 to m do
+    begin
+        k:=-32768;
         for j:=1 to n do
         begin
             write('a[',i,',',j,']:=');readln(a[i,j]);
+            if a[i,j]>k then k:=a[i,j];
+            if a[i,j]=x then inc(c);
         end;
+        s:=s+k;
+    end;
     for i:=1 to m do
     begin
         for j:=1 to n do
             write(a[i,j]:4);
         writeln;
     end;
+    writeln('Tong cac phan tu lon nhat cua moi dong:',s);
+    writeln('So lan xuat hien x trong mang:',c);
     readln;
 end.
