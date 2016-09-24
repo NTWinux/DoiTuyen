d5ae62b9a650a45f223173201568e800d5209b11
unknown <dnt2552@gmail.com>
2016-09-24 10:47:20 +0700

    Final Addition

--- p5.pas
+++ p5.pas
@@ -2,7 +2,9 @@
 uses crt;
 var 
     str,s:string[70];
-//    len,p,k:byte;
+    len,p,k:byte;
+    c:char;
+    Count:array ['A'..'Z'] of byte;
 begin
     write('Nhap Xau: ');readln(str);
 //    len:=length(str);
@@ -19,9 +21,13 @@ begin
 //    insert(s,str,p);
 //    writeln('Xau sau khi chen: ',str);
 //    p:=0;
-//    for p:=1 to length(s) Do
+//    for p:=1 to length(str) Do
 //        if (str[p] in ['0'..'9']) then inc(k);
 //    write(k);
-    write(Upcase(str));
+//    write(Upcase(str));
+    for k:=1 to length(str) do
+        if Upcase(str[k]) in ['A'..'Z'] then inc(Count[Upcase(str[k])]);
+    for c:='A' to 'Z' do 
+        if Count[c]<>0 then writeln(c,'-',Count[c]);
     readln;
 end.
