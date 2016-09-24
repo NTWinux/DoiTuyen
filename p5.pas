991988835b5d102e3059df5386ec154e091f8c02
unknown <dnt2552@gmail.com>
2016-09-24 08:57:01 +0700

    Add `Copy`

--- p5.pas
+++ p5.pas
@@ -2,10 +2,13 @@
 uses crt;
 var 
     str:string[15];
-    len:byte;
+    len,p,k:byte;
 begin
     write('Nhap Xau: ');readln(str);
     len:=length(str);
     write('Do dai cua xau: ',len);
+    write('Vi tri lay: ');readln(p);
+    write('So Luong: ');readln(k);
+    write(copy(str,p,k));
     readln;
 end.
