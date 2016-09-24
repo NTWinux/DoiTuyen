5ecb53861a11d93821d0ad4049d585f74971e014
unknown <dnt2552@gmail.com>
2016-09-24 08:52:10 +0700

    Add p5.pas

---
+++ p5.pas
@@ -0,0 +1,11 @@
+// Xau
+uses crt;
+var 
+    str:string[15];
+    len:byte;
+begin
+    write('Nhap Xau: ');readln(str);
+    len:=length(str);
+    write('Do dai cua xau: ',len);
+    readln;
+end.
