1663690ef26c55460e16fb80d4f560201cf55231
unknown <dnt2552@gmail.com>
2016-09-24 10:20:17 +0700

    Add

--- p5.pas
+++ p5.pas
@@ -1,22 +1,27 @@
 // Xau
 uses crt;
 var 
-    str,s:string[30];
-    len,p,k:byte;
+    str,s:string[70];
+//    len,p,k:byte;
 begin
     write('Nhap Xau: ');readln(str);
-    len:=length(str);
-    writeln('Do dai cua xau: ',len);
-    write('Vi tri lay: ');readln(p);
-    write('So Luong: ');readln(k);
-    writeln(copy(str,p,k));
-    write('Vi tri xoa: ');readln(p);
-    write('So Luong: ');readln(k);
-    delete(str,p,k);
-    writeln('Xau sau khi xoa ',str);
-    write('Vi tri chen: ');readln(p);
-    write('Xau can chen: ');readln(s);
-    insert(s,str,p);
-    writeln('Xau sau khi chen: ',str);
+//    len:=length(str);
+//    writeln('Do dai cua xau: ',len);
+//    write('Vi tri lay: ');readln(p);
+//    write('So Luong: ');readln(k);
+//    writeln(copy(str,p,k));
+//   write('Vi tri xoa: ');readln(p);
+//    write('So Luong: ');readln(k);
+//    delete(str,p,k);
+//   writeln('Xau sau khi xoa: ',str);
+//    write('Vi tri chen: ');readln(p);
+//    write('Xau can chen: ');readln(s);
+//    insert(s,str,p);
+//    writeln('Xau sau khi chen: ',str);
+//    p:=0;
+//    for p:=1 to length(s) Do
+//        if (str[p] in ['0'..'9']) then inc(k);
+//    write(k);
+    write(Upcase(str));
     readln;
 end.
